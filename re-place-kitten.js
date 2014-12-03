c48bea9995677658d0cb7ea509bf019b2d316f80
Luke Corpe <lukecorpe@semantico.com>
2014-12-03 17:34:00 +0000

    updated JS

--- re-place-kitten.js
+++ re-place-kitten.js
@@ -1,12 +1,25 @@
+function replace_image(element){
+    var src = 'http://placekitten.com/g/' + $(element).width() + '/' + $(element).height();
+    $(element).attr("src", src);
+}
+
 $(function() {
         var global = false;
-        if($('body[data-re-place-kitten]').length){
+        var replace_class = '';
+        if($('body[data-re-place-kitten-global]').length){
             global = true;
         }
+        if($('body[data-re-place-kitten]').length && !global){
+            replace_class = $('body[data-re-place-kitten]');
+        }
+
         $('img').load(function() {
-            if (global || $(this).attr('data-re-place-kitten') !== undefined && $(this).attr('data-re-place-kitten').length !== undefined) {
-                var src = 'http://placekitten.com/g/' + $(this).width() + '/' + $(this).height();
-                $(this).attr("src", src);
+            if (global) {
+                replace_image(this);
+            } else if($(this).attr('data-re-place-kitten') !== undefined && $(this).attr('data-re-place-kitten').length !== undefined){
+                replace_image(this);
+            }else if (replace_class !== '' && $(this).hasClass(replace_class)) {
+                replace_image(this);
             }
         });
 });
